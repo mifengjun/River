08e3c51e13bd00ccdfa29f4a7f691ed6d3ad4875
小米 <40954392+mifengjun@users.noreply.github.com>
2021-11-02 22:08:22 +0800

    自动化部署

--- deploy.sh
+++ deploy.sh
@@ -15,12 +15,17 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 echo 'tama.wiki' > CNAME
 
+// GITHUB_TOKEN
+gitUrl = https://mifengjun:${GITHUB_TOKEN}@github.com/mifengjun/mifengjun.github.io.git
+
 git init
 git add -A
 git commit -m 'deploy'
+git config --global user.name "mifengjun"
+git config --global user.email "522622782@qq.com"
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:mifengjun/mifengjun.github.io.git master
+git push -f $gitUrl master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
